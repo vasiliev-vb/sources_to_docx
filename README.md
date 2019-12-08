c06105dd4c578e1061137abbd65fe8cbd8722c6e
Vladimir Vasiliev <vasiliev.vb@gmail.com>
2019-12-08 20:13:47 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,66 @@
+sources\_to\_docx
+================
+
+Little script to write a bunch of files with source code to .docx.
+
+Requirements: python3, python-docx.
+
+
+Just listings (probably, ugly looking):
+```
+# List source files
+$ find some_project -name '*.cpp' -or -name '*.h' > some_project.txt
+$ find some_library -name '*.cpp' -or -name '*.h' > some_library.txt
+
+$ sources_to_docx.py --input *.txt
+                     --output code_listings.docx
+```
+
+
+Use template .docx file as reference and apply style named 'My\_code\_style' to listings:  
+```
+$ sources_to_docx.py --input *.txt
+                     --output code_listings.docx
+                     --template template.docx 
+                     --code_style 'My_code_style'
+```
+
+Use .docx file as reference, apply code style and print code instead of paragraph with text '<CODE>':
+```
+$ sources_to_docx.py --input *.txt
+                     --output document_with_code_listings.docx
+                     --template document.docx 
+                     --code_style 'My_code_style'
+                     --code_place_mark '<CODE>'
+```
+
+Print heading "Library \<file\_list\_name\>" for each file list,
+apply some custom styles:
+```
+$ sources_to_docx.py --input *.txt
+                     --output code_listings.docx
+                     --template template.docx 
+                     --code_style code
+                     --with_headings
+                     --heading_style 'Heading 3'
+                     --text_style 'My_text_style'
+                     --code_style 'My_code_style'
+                     --file_list_description 'Library {}'
+                     --file_description 'File {}:'
+```
+
+Usage of GOST 19.401-78 ("ЕСПД") template:
+```
+$ sources_to_docx.py --input *.txt
+                     --output code_listings.docx
+                     --template gost_espd/template.docx 
+
+                     --with_headings # Optional
+                     --heading_style 'td_toc_caption_level_1'
+                     --code_style td_code
+                     --text_style td_text
+
+                     --file_list_description 'Подпрограмма {}'
+                     --file_description 'Исходный модуль {}:'
+```
+
---
+++ sources_to_docx.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import argparse
+import docx
+import os
+
+def modify_document(d, file_lists, args):
+    codep = None
+    for paragraph in d.paragraphs:
+        if args.code_place_mark in paragraph.text:
+            codep = paragraph
+    if codep is not None:
+        add_paragraph = codep.insert_paragraph_before
+    else:
+        add_paragraph = d.add_paragraph
+        print("Paragraph with '{}' text was not found. Appending to end of file".format(args.code_place_mark))
+
+    p = None
+    for file_list in file_lists:
+        # insert page break after each paragraph except last one
+        if p is not None:
+            run = p.add_run()
+            run.add_break(docx.enum.text.WD_BREAK.PAGE)
+
+        if args.with_headings:
+            p = add_paragraph(args.file_list_description.format(os.path.splitext(file_list)[0]))
+            p.style = args.heading_style
+        with open(file_list,'r') as file_list_content:
+            for file_name in file_list_content:
+                file_name = file_name.strip()
+                p = add_paragraph(args.file_description.format(os.path.basename(file_name)))
+                p.style = args.text_style
+                with open(file_name,'r') as file_content:
+                    p = add_paragraph(file_content.read())
+                    p.style = args.code_style
+    if codep:
+        codep.text=''
+    return d
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Writes a bunch of text files to .docx')
+
+    parser.add_argument('--input','-i',            required=True, nargs='+', help='Input file list or multiple file lists. In latter case, hierarchy can be used')
+    parser.add_argument('--template','-t',         default=None,             help='Template .docx file with styles and preformatting')
+    parser.add_argument('--output', '-o',          required=True,            help='Output .docx file')
+
+    parser.add_argument('--code_place_mark',       default='<CODE>',         help='Paragraph with this mark will be replaced by code')
+                                                                             
+    parser.add_argument('--file_list_description', default='{}',             help='Description of file list')
+    parser.add_argument('--file_description',      default='{}:',            help='Description of file')
+                                                                             
+    parser.add_argument('--with_headings',         action='store_true',      help='Print headings for files')
+    parser.add_argument('--heading_style',         default='Heading 1',      help='Style for text')
+                                                                             
+    parser.add_argument('--text_style',            default='Normal',         help='Style for text')
+    parser.add_argument('--code_style',            default='Normal',         help='Style for code')
+                                                                             
+    parser.add_argument('--print_styles',          action='store_true',      help='Print styles available in template')
+
+    args = parser.parse_args()
+
+    if args.template is not None:
+        d = docx.Document(args.template)
+    else:
+        d = docx.Document()
+
+    if args.print_styles:
+        print('=================')
+        print('Styles available:')
+        for s in d.styles:
+            print(s.name) 
+        print('=================')
+        exit(0)
+
+    d = modify_document(d, args.input, args)
+    d.save(args.output)
+
